8f5836e6e220ec8376ecf8cb14c8befcb3a6f7db
Monali_Tayade <c-monali.tayade@timesgroup.com>
2025-02-06 18:53:09 +0530

    responsive menu

--- src/app/component/header/page.jsx
+++ src/app/component/header/page.jsx
@@ -1,37 +1,41 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { use, useState } from "react";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Logo from "@/assets/images/logo.svg";
 import Search from "@/assets/images/search.svg";
-import Profile from "@/assets/images/Profile.svg";
-import Cart from "@/assets/images/Cart.svg";
+import Profile from "@/assets/images/profile.svg";
+import Nav from "@/assets/images/nav.svg";
+import Notification from "@/assets/images/notification.svg";
 
-function Header() {
+function Header({ setMobMenu }) {
   const { data: session, status } = useSession();
 
   const router = useRouter();
 
-  //status for login/logout button
-  console.log("session", session?.user?.role, status);
+  const [logoutPopUp, setLogoutPopUp] = useState(false);
+
   const handleLogout = async (e) => {
     e.preventDefault();
     await signOut({ redirect: false }); // Prevent default redirect
     router.push("/login"); // Manually redirect to login page
   };
 
+  const logoutPopup = () => {
+    setLogoutPopUp((prev) => !prev);
+  };
+
   return (
     <header className="w-full flex flex-row bg-[#7978e9] py-3 px-2 shadow-slate-600">
+      <div className="lg:hidden flex w-10 mr-3">
+        <button className="w-full" onClick={() => setMobMenu(true)}>
+          <Image w={10} h={10} alt="navigation" src={Nav} />
+        </button>
+      </div>
       <div className="w-full flex items-center">
         <a href="/" className="text-black mr-3">
-          {/* <Img
-            src="/src/assets/images/logo.svg"
-            width={30}
-            height={30}
-            alt="logo"
-          /> */}
           <Image src={Logo} width={30} height={30} alt="logo" />
         </a>
 
@@ -39,61 +43,27 @@ function Header() {
           Task Tracker
         </div>
       </div>
-      {/* {session?.user?.role === "admin" && (
-        <a
-          href="/assign-task"
-          className="ml-auto bg-white rounded-sm px-5 py-2 text-red-300">
-          Assign Task
-        </a>
-      )} */}
+
+      <button className="ml-4 py-0 text-white  relative">
+        <Image width={30} height={30} alt="profile" src={Notification} />
+      </button>
+
       {session && (
         <button
-          onClick={(e) => handleLogout(e)}
-          className="ml-4 py-0 text-white px-4 ">
-          Logout
+          onClick={() => logoutPopup()}
+          className="ml-3 py-0 text-white relative">
+          <Image width={30} height={30} alt="profile" src={Profile} />
+          {logoutPopUp && (
+            <div className="w-[200px] absolute top-[35px] right-[21px] bg-white p-3 shadow-2xl">
+              <div
+                className="text-[#7978e9] cursor-pointer font-sans font-semibold"
+                onClick={(e) => handleLogout(e)}>
+                Logout
+              </div>
+            </div>
+          )}
         </button>
       )}
-      {/* <nav className="w-full flex flex-row justify-between items-center">
-        <ul className="flex">
-          <li className="flex px-4">
-            <a href="/" className="text-black text-sm">
-              Men
-            </a>
-          </li>
-          <li className="flex px-4">
-            <a href="/" className="text-black text-sm">
-              Women
-            </a>
-          </li>
-          <li className="flex px-4">
-            <a href="/" className="text-black text-sm">
-              Kids
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div className="w-auto flex flex-row items-center">
-        <div
-          className="w-auto flex flex-row justify-between  rounded-full"
-          dir="ltr">
-          <input
-            type="text"
-            name="search"
-            className="text-black px-3 py-2 focus:outline-none border-[1px] rounded-s-full"
-          />
-          <button className="w-[30px] border rounded-s-full p-[3px]" dir="rtl">
-            <Image src={Search} width={30} height={30} alt="logo" />
-          </button>
-        </div>
-        <div className="w-auto flex flex-row">
-          <div className="w-[28px] mx-2">
-            <Image src={Profile} width={28} height={28} alt="logo" />
-          </div>
-          <div className="w-[28px] mx-2">
-            <Image src={Cart} width={28} height={28} alt="logo" />
-          </div>
-        </div>
-      </div> */}
     </header>
   );
 }
--- src/app/component/layoutwrapper/page.jsx
+++ src/app/component/layoutwrapper/page.jsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../header/page";
 import Sidenav from "../sidenav/page";
 import { usePathname } from "next/navigation";
 
 const page = ({ children }) => {
   const pathname = usePathname();
+
+  const [mobMenu, setMobMenu] = useState(false);
 
   console.log("pathname", pathname);
 
@@ -14,14 +16,16 @@ const page = ({ children }) => {
 
   return (
     <div className="flex flex-col">
-      {!hideComponent && <Header />}
+      {!hideComponent && <Header setMobMenu={setMobMenu} />}
       <div className="flex w-full">
         {!hideComponent && (
-          <div className="w-[23%]">
+          <div className={`w-[23%] lg:flex hidden`}>
             <Sidenav />
           </div>
         )}
-        <div className="w-[100%] min-h-[90.5vh]">{children}</div>
+        <div className="w-[100%] lg:min-h-[90.5vh] sm:min-h-[unset]">
+          {children}
+        </div>
       </div>
     </div>
   );
